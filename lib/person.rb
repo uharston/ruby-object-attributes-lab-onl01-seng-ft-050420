ba26a69523f89189bdd1bfad1785bbb85a594765
Uriah Harston <uharston@gmail.com>
2020-05-11 21:09:52 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(name)
+    @persons_name = name
+  end
+  def name
+    @persons_name
+  end
+  def job=(job)
+    @persons_job = job
+  end
+  def job
+    @persons_job
+  end
+    
+end

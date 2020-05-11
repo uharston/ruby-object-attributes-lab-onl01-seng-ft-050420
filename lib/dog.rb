79db75c561e0623dd4c489d7d52a42478b06c49a
Uriah Harston <uharston@gmail.com>
2020-05-11 21:16:08 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(dogs_name, dogs_breed)
+    @this_dogs_name = dogs_name
+    @this_dogs_breed = dogs_breed
+  end
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  def name
+    @this_dogs_name
+  end
+  def breed=(dogs_breed)
+    @this_dogs_breed = dogs_breed
+  end
+  def breed
+    @this_dogs_breed
+  end
+end
